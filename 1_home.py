d5c85c09234bd268e73c8757195c9e2f5db4f9e5
vinight10 <vinicius.fiorezi@gmail.com>
2023-10-16 03:37:57 -0300

    requ

--- 1_home.py
+++ 1_home.py
@@ -12,7 +12,7 @@ st.set_page_config(
     layout='wide'
 )
 
-st.header('Calculadora de Sistemas Trifásicos')
+st.header('Calculadora de Sistemas Trifásicos', divider='rainbow')
 ''
 st.subheader('Faça cálculos de sistemas trifásicos de forma fácil e interativa')
 ''
