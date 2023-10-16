7882c172ce65c321bc123c507b6e73dbfa88eaf6
vinight10 <vinicius.fiorezi@gmail.com>
2023-10-16 03:12:43 -0300

    att

--- 1_home.py
+++ 1_home.py
@@ -19,7 +19,7 @@ st.subheader('Faça cálculos de sistemas trifásicos de forma fácil e interati
 ''
 'Este site tem como intuito auxiliar os estudantes de engenharia elétrica nas disciplinas de circuitos elétricos, a fim de facilitar o processo de aprendizagem trazendo os gráficos de fasores de tensão e de corrente sem complicações.'
 ''
-'Projeto desenvolvido como trabalho de conclusão de curso de engenharia na Uniftec - Caxias do Sul'
+'Projeto desenvolvido como trabalho de conclusão de curso de engenharia elétrica na Uniftec - Caxias do Sul'
 ''
 'Desenvolvido por: Vinicius Martins Fioreze - Email: vinicius.fioreze@acad.ftec.com.br'
 'Professor orientador: Esp. Jocemar Muller Boeira '
