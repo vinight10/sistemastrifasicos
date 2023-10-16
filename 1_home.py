5262009d83f7c289a720796c96ef044d01132af9
vinight10 <vinicius.fiorezi@gmail.com>
2023-10-15 22:57:35 -0300

    email

--- 1_home.py
+++ 1_home.py
@@ -21,5 +21,5 @@ st.subheader('Faça cálculos de sistemas trifásicos de forma fácil e interati
 ''
 'Projeto desenvolvido como trabalho de conclusão de curso de engenharia na Uniftec - Caxias do Sul'
 ''
-'Desenvolvido por: Vinicius Martins Fioreze'
+'Desenvolvido por: Vinicius Martins Fioreze - Email: vinicius.fiorezi@gmail.com'
 'Professor orientador: Esp. Jocemar Muller Boeira '
