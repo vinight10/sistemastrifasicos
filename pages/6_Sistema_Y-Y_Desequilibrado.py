262e29541b0e40bba28bec3537a40925c5fc022c
vinight10 <vinicius.fiorezi@gmail.com>
2023-10-23 01:12:58 -0300

    kljh

---
+++ pages/6_Sistema_Y-Y_Desequilibrado.py
@@ -0,0 +1,13 @@
+import math
+import cmath
+import numpy as np
+import matplotlib.pyplot as plt
+import streamlit as st
+from PIL import Image
+
+
+
+st.header('Calculadora de Sistemas Trifásicos :star:')
+
+st.subheader('Em breve disponível!!!')
+
